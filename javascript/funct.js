9e19a2bbf274262004c14a2cf3edb7e2e3ee9192
JoDaBaRo <josdabayona@gmail.com>
2018-09-27 07:52:32 -0500

    Adding images to columns

--- javascript/funct.js
+++ javascript/funct.js
@@ -1,8 +1,12 @@
 window.onload=function(){
-	$("#contendio").load("../ejemplos.html");
+	$("#contendio").load("../ejemplos.html", function(){
+		alert("Bienvenido al laboratorio SQL")
+	});
 };
 $("li a").click(function(event){
-		$("#contendio").load("../ejercicios.html");
+		$("#contendio").load("../ejercicios.html", function(){
+			alert("Cargando ejercicios")
+		});
 });
 
 
